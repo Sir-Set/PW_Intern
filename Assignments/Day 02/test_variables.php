ebd46976a35c10f5be683808a3228d40afb85217
Tashu Sethi <159637099+Sir-Set@users.noreply.github.com>
2026-06-13 04:22:34 +0530

    Add files via upload

---
+++ Assignments/Day 02/test_variables.php
@@ -0,0 +1,21 @@
+<?php
+$name = "Tashu";
+$age = 21;
+$city = "Chandigarh";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Variables</title>
+</head>
+<body>
+
+<h2>PHP Variables</h2>
+
+<p>Name: <?php echo $name; ?></p>
+<p>Age: <?php echo $age; ?></p>
+<p>City: <?php echo $city; ?></p>
+
+</body>
+</html>
